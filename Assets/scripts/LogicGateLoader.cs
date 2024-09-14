fa7a3e5643e9c4c0b1a5a9dacb0393731f543e05
calebEbenkamp <calebebenkamp10@gmail.com>
2024-09-13 21:53:31 -0500

    Level 2 Scene Update
    Added platforms to level 2 along with text and a button to take you to level 2 from level 1

---
+++ Assets/scripts/LogicGateLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level1Loader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player"))
+            SceneManager.LoadSceneAsync(2);
+    }  
+} 

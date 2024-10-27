dbb17aa4b74b653d9f897366cd60951c58912cd0
calebEbenkamp <calebebenkamp10@gmail.com>
2024-10-26 20:52:26 -0500

    Binary level
    Gate doors don't spin anymore

--- Assets/scripts/BinaryButtons/BinaryAdderArray.cs
+++ Assets/scripts/BinaryButtons/BinaryAdderArray.cs
@@ -1,33 +1,3 @@
-// using UnityEngine;
-// using TMPro;
-
-// public class BinaryArrayAdder : MonoBehaviour
-// {
-//     public BinaryButtonArray array1; // Reference to the first BinaryButtonArray
-//     public BinaryButtonArray array2; // Reference to the second BinaryButtonArray
-//     public TMP_Text outputText; // Text field to display the result
-
-//     private int binarySum; // Holds the calculated sum of the two arrays
-
-//     // Method to update the output by adding the two arrays' decimal values
-//     public void UpdateSumOutput()
-//     {
-//         if (array1 != null && array2 != null && outputText != null)
-//         {
-//             int decimalValue1 = array1.GetDecimalValue();
-//             int decimalValue2 = array2.GetDecimalValue();
-//             binarySum = decimalValue1 + decimalValue2;
-
-//             outputText.text = "Sum: " + binarySum;
-//         }
-//     }
-
-//     public int GetOutputValue()
-//     {
-//         return binarySum; // Return the calculated sum
-//     }
-// }
-
 using UnityEngine;
 using TMPro;
 

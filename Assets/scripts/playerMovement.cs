0c065c5814545b6df5409789a160060136b1e406
calebEbenkamp <calebebenkamp10@gmail.com>
2024-10-29 19:29:34 -0500

    Binary Array now works as an OnCollision instead of an OnTrigger

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -1,80 +1,3 @@
-// using UnityEngine;
-
-// public class playerMovement : MonoBehaviour
-// {
-//     public float speed;
-//     public float jump;
-//     float horizontalInput;
-//     private Rigidbody2D rb;
-//     private float move;
-//     private bool isJumping = false;
-//     private Vector3 respawnPoint;
-
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//         respawnPoint = transform.position;
-//     }
-
-//     //update is called once per frame
-//     void Update()
-//     {
-//         //flip left to right -> not done
-//         horizontalInput = Input.GetAxisRaw("Horizontal");
-
-//         if(horizontalInput > 0)
-//         {
-//             gameObject.transform.localScale = new Vector3(2, 2, 1);
-//         }
-//         if(horizontalInput < 0)
-//         {
-//             gameObject.transform.localScale = new Vector3(-2, 2, 1);
-//         }
-
-//         //jumping
-//         if(Input.GetKey(KeyCode.W) && !isJumping)
-//         {
-//             rb.velocity = new Vector2(rb.velocity.x, jump);
-//             isJumping = true;
-//         }    
-//         move = Input.GetAxis("Horizontal");
-//         rb.velocity = new Vector2(move * speed, rb.velocity.y);
-//     }
-
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if (other.CompareTag("Button"))
-//         {
-//             // Retrieve the button's ButtonIndex component
-//             ButtonIndex buttonIndex = other.GetComponent<ButtonIndex>();
-
-//             if (buttonIndex != null && buttonIndex.buttonArray != null)
-//             {
-//                 // Get the index and the associated BinaryButtonArray
-//                 int index = buttonIndex.index;
-//                 BinaryButtonArray buttonArrayManager = buttonIndex.buttonArray;
-
-//                 // Toggle the binary value for this button in the correct array using the arrayID
-//                 buttonArrayManager.ToggleBinaryValue(index, buttonArrayManager.arrayID);
-//             }
-//         }
-//         else if(other.CompareTag("Checkpoint")){
-//             respawnPoint = transform.position;
-//         }
-//         else if(other.CompareTag("Lava")){
-//             transform.position = respawnPoint;
-//         }
-//     }
-
-//     private void OnCollisionEnter2D(Collision2D other)
-//     {
-//         if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform"))
-//         {
-//             isJumping = false;
-//         }
-//     }
-// }
-
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -120,11 +43,7 @@ public class playerMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Button"))
-        {
-            HandleButtonInteraction(other);
-        }
-        else if (other.CompareTag("Checkpoint"))
+        if (other.CompareTag("Checkpoint"))
         {
             respawnPoint = transform.position;
         }
@@ -136,25 +55,25 @@ public class playerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform"))
+        if (other.gameObject.CompareTag("Button"))
+        {
+            HandleButtonInteraction(other.collider);
+        }
+        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform") || other.gameObject.CompareTag("Button"))
         {
             isJumping = false;
         }
     }
 
-    // New method for handling button interactions
     private void HandleButtonInteraction(Collider2D buttonCollider)
     {
-        // Retrieve the button's ButtonIndex component
         ButtonIndex buttonIndex = buttonCollider.GetComponent<ButtonIndex>();
 
         if (buttonIndex != null && buttonIndex.buttonArray != null)
         {
-            // Get the index and the associated BinaryButtonArray
             int index = buttonIndex.index;
             BinaryButtonArray buttonArrayManager = buttonIndex.buttonArray;
 
-            // Toggle the binary value for this button in the correct array using the arrayID
             buttonArrayManager.ToggleBinaryValue(index, buttonArrayManager.arrayID);
         }
     }

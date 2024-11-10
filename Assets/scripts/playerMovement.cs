f5380fa9bb0629fa3114539ba20e1bfbb0696aa8
FireMysty <125101747+FireMysty@users.noreply.github.com>
2024-11-09 20:31:05 -0600

    First Enemy!

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -13,6 +14,16 @@ public class playerMovement : MonoBehaviour
     [SerializeField] private LayerMask wallLayer;
     private float wallJumpCooldown;
     private float horizontalInput;
+    // Wall jumping/ sliding
+    private bool isFacingRight = true;
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 12f);
 
     void Start()
     {
@@ -28,55 +39,78 @@ public class playerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         
         // Horizontal movement
-        if (horizontalInput > 0)
-        {
-            gameObject.transform.localScale = new Vector3(3, 3, 1);
-        }
-        if (horizontalInput < 0)
-        {
-            gameObject.transform.localScale = new Vector3(-3, 3, 1);
-        }
+        // if (horizontalInput > 0)
+        // {
+        //     gameObject.transform.localScale = new Vector3(3, 3, 1);
+        // }
+        // if (horizontalInput < 0)
+        // {
+        //     gameObject.transform.localScale = new Vector3(-3, 3, 1);
+        // }
 
         // Jumping
         if(isGrounded() && Input.GetKeyDown(KeyCode.W))
             Jump();
-        if(wallJumpCooldown > 0)
-            wallJumpCooldown -= Time.deltaTime;
-        else if(wallJumpCooldown <= 0)
-        {
-            rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        
+        wallSlide();
+        wallJump();
 
-            if(onWall() && !isGrounded() && Input.GetKeyDown(KeyCode.W))
-                wallJump();
-
-            if(onWall() && !isGrounded())
-            {
-                rb.gravityScale = 0;
-                rb.velocity = Vector2.zero;
-            }
-            else
-                rb.gravityScale = 2;
-        }
+        if(!isWallJumping)
+            Flip();
 
         // animations
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
     }
+
+    private void FixedUpdate()
+    {
+        if(!isWallJumping)
+        {   // while walljumping, player cannot move horizontally until wallJumpDuration becomes 0
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y); //move horizontally
+        }
+    }
+
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jump); // Apply jump velocity
         anim.SetTrigger("jump");
     }
-    private void wallJump()
+  private void wallJump()
     {
-        if(onWall() && !isGrounded() && wallJumpCooldown <= 0)
+        if(isWallSliding)
         {
-            wallJumpCooldown = 0.2f;
-            float wallJumpDirection = Mathf.Sign(transform.localScale.x) * -1;
-            rb.velocity = new Vector2(wallJumpDirection * 3, 6);
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime; //
 
-            anim.SetTrigger("jump");
+            CancelInvoke(nameof(StopWallJumping));
+
         }
+        else 
+        {
+            wallJumpingCounter -= Time.deltaTime; //this allows us to turn away from the wall and be able to walljump for a brief moment
+        }
+        if(Input.GetKeyDown(KeyCode.W) && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+        }
+        Invoke(nameof(StopWallJumping), wallJumpingDuration);
+    }
+
+     private void StopWallJumping()
+    {
+        isWallJumping = false;
     }
 
     private bool isGrounded()
@@ -88,6 +122,31 @@ public class playerMovement : MonoBehaviour
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+
+        private void wallSlide() 
+    {
+        //if player is touching wall (isWallSliding = true), not touching ground, and is moving horizontally(pushing into wall)
+        if (onWall() && !isGrounded() && horizontalInput != 0f)
+        {
+            //while wallsliding, "clamp" the rigidbody's y velocity between negative value of wallslidespeed(2 as of now)
+            // and the float.Maxvalue
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else {
+            isWallSliding = false;
+        }
+    }
+     private void Flip()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)

0286479c607518e8d73f04ad668688b91acbc6fa
FireMysty <125101747+FireMysty@users.noreply.github.com>
2024-11-15 13:46:43 -0600

    Deleting current cnflicting errors

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -1,3 +1,244 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // using System;
 // using UnityEngine;
 
@@ -5,7 +246,10 @@
 // {
 //     public float speed;
 //     public float jump;
+//     private float horizontalInput;
 //     private Rigidbody2D rb;
+//     private float move;
+//     private bool isJumping = false;
 //     private Vector3 respawnPoint;
 //     private Animator anim;
 //     private BoxCollider2D boxCollider;
@@ -18,13 +262,11 @@
 //     {
 //         rb = GetComponent<Rigidbody2D>();
 //         respawnPoint = transform.position;
-//         anim = GetComponent<Animator>();
-//         boxCollider = GetComponent<BoxCollider2D>();
 //     }
 
-//     // Update is called once per frame
 //     void Update()
 //     {
+//         // Flip left to right
 //         horizontalInput = Input.GetAxisRaw("Horizontal");
         
 //         // Horizontal movement
@@ -90,12 +332,41 @@
 //         return raycastHit.collider != null;
 //     }
 
+//         private void wallSlide() 
+//     {
+//         //if player is touching wall (isWallSliding = true), not touching ground, and is moving horizontally(pushing into wall)
+//         if (onWall() && !isGrounded() && horizontalInput != 0f)
+//         {
+//             //while wallsliding, "clamp" the rigidbody's y velocity between negative value of wallslidespeed(2 as of now)
+//             // and the float.Maxvalue
+//             isWallSliding = true;
+//             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+//         }
+//         else {
+//             isWallSliding = false;
+//         }
+//     }
+//      private void Flip()
+//     {
+//         if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+//         {
+//             Vector3 localScale = transform.localScale;
+//             isFacingRight = !isFacingRight;
+//             localScale.x *= -1f;
+//             transform.localScale = localScale;
+//         }
+//     }
+
 //     private void OnTriggerEnter2D(Collider2D other)
 //     {
 //         if (other.CompareTag("Checkpoint"))
+//         {
 //             respawnPoint = transform.position;
+//         }
 //         else if (other.CompareTag("Lava"))
+//         {
 //             transform.position = respawnPoint;
+//         }
 //     }
 
 //     private void OnCollisionEnter2D(Collision2D other)
@@ -104,18 +375,24 @@
 //         {
 //             HandleButtonInteraction(other.collider);
 //         }
-//         /*if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform") || other.gameObject.CompareTag("Button"))
+//         if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform") || other.gameObject.CompareTag("Button"))
 //         {
-//             //isJumping = false;
-//         }*/
+//             isJumping = false;
+//         }
 
 //         RepresentationTypeChanger representationChanger = other.gameObject.GetComponent<RepresentationTypeChanger>();
 //         if (representationChanger != null)
 //         {
+//             // Cycle the representation type
 //             representationChanger.CycleType();
+
+//             // Update the sum output in BinaryArrayAdder
+//             if (binaryArrayAdder != null)
+//             {
+//                 binaryArrayAdder.UpdateSumOutput();
+//             }
 //         }
 //     }
-
 
 //     private void HandleButtonInteraction(Collider2D buttonCollider)
 //     {
@@ -130,99 +407,3 @@
 //         }
 //     }
 // }
-
-using UnityEngine;
-
-public class playerMovement : MonoBehaviour
-{
-    public float speed;
-    public float jump;
-    private float horizontalInput;
-    private Rigidbody2D rb;
-    private float move;
-    private bool isJumping = false;
-    private Vector3 respawnPoint;
-
-    // Reference to BinaryArrayAdder for updating the sum
-    public BinaryArrayAdder binaryArrayAdder;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        respawnPoint = transform.position;
-    }
-
-    void Update()
-    {
-        // Flip left to right
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-
-        if (horizontalInput > 0)
-        {
-            gameObject.transform.localScale = new Vector3(2, 2, 1);
-        }
-        if (horizontalInput < 0)
-        {
-            gameObject.transform.localScale = new Vector3(-2, 2, 1);
-        }
-
-        // Jumping
-        if (Input.GetKey(KeyCode.W) && !isJumping)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jump);
-            isJumping = true;
-        }
-        move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(move * speed, rb.velocity.y);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Checkpoint"))
-        {
-            respawnPoint = transform.position;
-        }
-        else if (other.CompareTag("Lava"))
-        {
-            transform.position = respawnPoint;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Button"))
-        {
-            HandleButtonInteraction(other.collider);
-        }
-        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform") || other.gameObject.CompareTag("Button"))
-        {
-            isJumping = false;
-        }
-
-        RepresentationTypeChanger representationChanger = other.gameObject.GetComponent<RepresentationTypeChanger>();
-        if (representationChanger != null)
-        {
-            // Cycle the representation type
-            representationChanger.CycleType();
-
-            // Update the sum output in BinaryArrayAdder
-            if (binaryArrayAdder != null)
-            {
-                binaryArrayAdder.UpdateSumOutput();
-            }
-        }
-    }
-
-    private void HandleButtonInteraction(Collider2D buttonCollider)
-    {
-        ButtonIndex buttonIndex = buttonCollider.GetComponent<ButtonIndex>();
-
-        if (buttonIndex != null && buttonIndex.buttonArray != null)
-        {
-            int index = buttonIndex.index;
-            BinaryButtonArray buttonArrayManager = buttonIndex.buttonArray;
-
-            buttonArrayManager.ToggleBinaryValue(index, buttonArrayManager.arrayID);
-        }
-    }
-}

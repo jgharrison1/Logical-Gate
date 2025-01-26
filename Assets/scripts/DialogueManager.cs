7c5f06e2c1d6c02c00007140a37eed0a700ef4ef
jgharrison1 <jasongh3711@yahoo.com>
2025-01-25 22:44:15 -0600

    Dialogue Box Pop-ups
    I have added 3 new scripts to implement dialogue boxes popping up in the current tutorial LGTL-J as well as an animation of it sliding up rather than popping up instantaneously. Next update, I want to add in the dialogue scripts and triggers, possibly a character for the professor in the dialogue window, and add animations for the truth tables and logic gates as the player goes through the tutorial.

---
+++ Assets/scripts/DialogueManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+    public GameObject dialogueBox;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        dialogueBox.SetActive(true);
+        animator.SetBool("IsOpen", true);
+        Debug.Log("Starting Dialogue with " + dialogue.name); //Remove Later
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        //check if there are more sentences left in the queue
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence); //Remove Later
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        //the purpose of this async function is to append dialogue letter by letter instead of all at once,
+        //which is more asthetically pleasing to read.
+        dialogueText.text = "";
+        //ToCharArray() converts string to char array, then append letters to dialogueText
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return null; // this waits for 1 frame after appending letter
+        }
+    }
+
+    public void EndDialogue() {
+        Debug.Log("End of Conversation");
+        animator.SetBool("IsOpen", false);
+        dialogueBox.SetActive(false);
+    }
+
+}

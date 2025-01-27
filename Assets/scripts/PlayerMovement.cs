a10831728199e2abc90e01241120eb8586d90e7f
calebEbenkamp <calebebenkamp10@gmail.com>
2025-01-27 11:26:55 -0600

    Implementation draft
    Can place blocks and grab placed block.

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,254 +1,3 @@
-// using System;
-// using UnityEditor.Tilemaps;
-// using UnityEngine;
-
-// public class playerMovement : MonoBehaviour, IDataPersistence
-// {
-//     public float speed;
-//     public float jump;
-//     private Rigidbody2D rb;
-//     private Vector3 respawnPoint;
-//     private Animator anim;
-//     private BoxCollider2D boxCollider;
-//     [SerializeField] private LayerMask groundLayer;
-//     [SerializeField] private LayerMask wallLayer;
-//     private float wallJumpCooldown;
-//     private float horizontalInput;
-//     private bool isFacingRight = true;
-//     private bool isWallSliding;
-//     private float wallSlidingSpeed = 1.5f;
-//     private bool isWallJumping;
-//     private float wallJumpingDirection;
-//     private float wallJumpingTime = 0.4f;
-//     private float wallJumpingCounter;
-//     private float wallJumpingDuration = 0.4f;
-//     private Vector2 wallJumpingPower = new Vector2(2.2f, 12f);
-//     private BinaryArrayAdder binaryArrayAdder;
-//     public float knockbackForce = 5f; // Knockback force for when colliding with the boss
-//     [SerializeField] private AudioSource walkSFX;
-//     [SerializeField] private GameObject startSpawn;
-
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//         respawnPoint = transform.position;
-//         anim = GetComponent<Animator>();
-//         boxCollider = GetComponent<BoxCollider2D>();
-//         walkSFX = GetComponent<AudioSource>();
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         horizontalInput = Input.GetAxisRaw("Horizontal");
-
-//         // Jumping
-//         if(isGrounded() && Input.GetKeyDown(KeyCode.W))
-//             Jump();
-        
-//         wallSlide();
-//         wallJump();
-
-//         if(!isWallJumping)
-//             Flip();
-
-//         // animations
-//         anim.SetBool("run", horizontalInput != 0);
-//         anim.SetBool("grounded", isGrounded());
-//         //play walk sound if grounded and walking
-//         if(rb.velocity.x!=0 && isGrounded())
-//         {
-//             if(!walkSFX.isPlaying)
-//                 walkSFX.Play();
-//         }
-//         else
-//         {
-//             walkSFX.Stop();
-//         }
-//     }
-
-//     private void FixedUpdate()
-//     {
-//         if(!isWallJumping)
-//         {   // while walljumping, player cannot move horizontally until wallJumpDuration becomes 0
-//             rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y); //move horizontally
-//         }
-//     }
-
-//     private void Jump()
-//     {
-//         rb.velocity = new Vector2(rb.velocity.x, jump); // Apply jump velocity
-//         anim.SetTrigger("jump");
-//     }
-//     private void wallJump()
-//     {
-//         if(isWallSliding)
-//         {
-//             isWallJumping = false;
-//             wallJumpingDirection = -transform.localScale.x;
-//             wallJumpingCounter = wallJumpingTime; //
-
-//             CancelInvoke(nameof(StopWallJumping));
-
-//         }
-//         else 
-//         {
-//             wallJumpingCounter -= Time.deltaTime; //this allows us to turn away from the wall and be able to walljump for a brief moment
-//         }
-//         if(Input.GetKeyDown(KeyCode.W) && wallJumpingCounter > 0f)
-//         {
-//             isWallJumping = true;
-//             rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
-//             wallJumpingCounter = 0f;
-
-//             if (transform.localScale.x != wallJumpingDirection)
-//             {
-//                 isFacingRight = !isFacingRight;
-//                 Vector3 localScale = transform.localScale;
-//                 localScale.x *= -1f;
-//                 transform.localScale = localScale;
-//             }
-//         }
-//         Invoke(nameof(StopWallJumping), wallJumpingDuration);
-//     }
-
-//      private void StopWallJumping()
-//     {
-//         isWallJumping = false;
-//     }
-
-//     private bool isGrounded()
-//     {
-//         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-//         return raycastHit.collider != null;
-//     }
-//     private bool onWall()
-//     {
-//         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
-//         return raycastHit.collider != null;
-//     }
-
-//         private void wallSlide() 
-//     {
-//         //if player is touching wall (isWallSliding = true), not touching ground, and is moving horizontally(pushing into wall)
-//         if (onWall() && !isGrounded() && horizontalInput != 0f)
-//         {
-//             //while wallsliding, "clamp" the rigidbody's y velocity between negative value of wallslidespeed(2 as of now)
-//             // and the float.Maxvalue
-//             isWallSliding = true;
-//             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
-//         }
-//         else {
-//             isWallSliding = false;
-//         }
-//     }
-//      private void Flip()
-//     {
-//         if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
-//         {
-//             Vector3 localScale = transform.localScale;
-//             isFacingRight = !isFacingRight;
-//             localScale.x *= -1f;
-//             transform.localScale = localScale;
-//         }
-//     }
-
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if (other.CompareTag("Checkpoint")) {
-//             Debug.Log("Checkpoint activated");
-//             respawnPoint = transform.position;
-//             DataPersistenceManager.instance.SaveGame();
-//         }
-//     }
-
-//     private void OnCollisionEnter2D(Collision2D other)
-//     {
-//         if (other.gameObject.CompareTag("Button"))
-//         {
-//             HandleButtonInteraction(other.collider);
-//         }
-
-//         if (other.gameObject.CompareTag("WormBoss"))
-//         {
-//             HandleBossCollision(other.gameObject);
-//         }
-
-//         // Handle representation change if applicable
-//         RepresentationTypeChanger representationChanger = other.gameObject.GetComponent<RepresentationTypeChanger>();
-//         if (representationChanger != null)
-//         {
-//             representationChanger.CycleType();
-
-//             if (binaryArrayAdder != null)
-//             {
-//                 binaryArrayAdder.UpdateSumOutput();
-//             }
-//         }
-
-//         // Change the segment's binary value when the player hits a segment
-//         WormSegment wormSegment = other.gameObject.GetComponent<WormSegment>();
-//         if (wormSegment != null)
-//         {
-//             wormSegment.ToggleBinaryValue();
-//         }
-
-//     }
-
-//     private void HandleBossCollision(GameObject boss)
-//     {
-//         WormBoss wormBoss = boss.GetComponent<WormBoss>();
-
-//         if (wormBoss != null)
-//         {
-
-//         }
-//     }
-
-//     private void HandleButtonInteraction(Collider2D buttonCollider)
-//     {
-//         ButtonIndex buttonIndex = buttonCollider.GetComponent<ButtonIndex>();
-
-//         if (buttonIndex != null && buttonIndex.buttonArray != null)
-//         {
-//             int index = buttonIndex.index;
-//             BinaryButtonArray buttonArrayManager = buttonIndex.buttonArray;
-
-//             buttonArrayManager.ToggleBinaryValue(index, buttonArrayManager.arrayID);
-//         }
-//     }
-//     public void Respawn()
-//     {
-//         transform.position = respawnPoint;
-
-//         Health health = GetComponent<Health>();
-//         if (health != null)
-//         {
-//             health.RestoreHealth();
-//         }
-//     }
-
-
-//     public void LoadData(GameData data) 
-//     {
-//         if(data.scenesVisited.TryGetValue(data.currentScene, out Vector3 prevPosition)) //if scene has been visited, update player position to position saved
-//         {
-//             this.transform.position = prevPosition;
-//             this.respawnPoint = data.respawnPoint;
-//         }
-//         //if scene has not been visited, keep player's default position
-//     }
-
-//     public void SaveData(GameData data) 
-//     {
-//         data.respawnPoint = this.respawnPoint;
-//         if (data.scenesVisited.ContainsKey(data.currentScene)) {
-//             data.scenesVisited.Remove(data.currentScene);
-//         }
-//         data.scenesVisited.Add(data.currentScene, this.transform.position);
-//     }
-// }
-
 
 using System;
 using UnityEditor.Tilemaps;
@@ -284,6 +33,14 @@ public class playerMovement : MonoBehaviour, IDataPersistence
     private GameObject heldBlock = null; // The block currently being held
     [SerializeField] private float grabDistance = 1.5f; // Distance to detect blocks
     [SerializeField] private LayerMask blockLayer; // Layer for blocks
+    [SerializeField] private LayerMask slotLayer; // Layer for blocks
+
+    private secondaryMemory secondaryMemoryInstance;
+
+    private GameObject highlightedSlot; // Currently highlighted slot
+    private GameObject highlightedBorder; // Border for the highlight
+    [SerializeField] private GameObject highlightBorderPrefab; // Prefab for the border
+
 
     void Start()
     {
@@ -292,6 +49,13 @@ public class playerMovement : MonoBehaviour, IDataPersistence
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
         walkSFX = GetComponent<AudioSource>();
+
+        secondaryMemoryInstance = FindObjectOfType<secondaryMemory>();
+
+        if (secondaryMemoryInstance == null)
+        {
+            Debug.LogError("No secondaryMemory instance found in the scene!");
+        }
     }
 
     // Update is called once per frame
@@ -323,7 +87,9 @@ public class playerMovement : MonoBehaviour, IDataPersistence
             walkSFX.Stop();
         }
 
+        HandleMovement();
         HandleBlockInteraction();
+        HighlightSlot();
     }
 
     private void FixedUpdate()
@@ -477,6 +243,13 @@ public class playerMovement : MonoBehaviour, IDataPersistence
         }
     }
 
+
+    private void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+    }
+
     private void HandleBlockInteraction()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -494,39 +267,199 @@ public class playerMovement : MonoBehaviour, IDataPersistence
 
     private void TryGrabBlock()
     {
-        // Detect blocks in range
-        Collider2D blockCollider = Physics2D.OverlapCircle(transform.position, grabDistance, blockLayer);
-        if (blockCollider != null)
-        {
-            GameObject block = blockCollider.gameObject;
+        // Detect blocks in range (use the blockLayer to filter)
+        Collider2D detectedCollider = Physics2D.OverlapCircle(transform.position, grabDistance, blockLayer);
 
-            // Pick up the block
+        if (detectedCollider != null)
+        {
+            // Get the block object
+            GameObject block = detectedCollider.gameObject;
+
+            // Grab the block and place it above the player
             heldBlock = block;
-            block.transform.SetParent(holdPosition);
-            block.transform.localPosition = Vector3.zero;
-            block.GetComponent<Rigidbody2D>().isKinematic = true; // Disable physics for the block
+            block.transform.SetParent(transform); // Make the block a child of the player
+            block.transform.position = transform.position + Vector3.up; // Place above the player
+            block.SetActive(true); // Make sure the block is active
+            Debug.Log("Grabbed block: " + block.name);
+        }
+        else
+        {
+            Debug.Log("No block detected to grab.");
         }
     }
 
     private void TryPlaceBlock()
     {
-        // Detect if the player is near a valid slot
-        Collider2D slotCollider = Physics2D.OverlapCircle(transform.position, grabDistance, blockLayer);
-        if (slotCollider != null && slotCollider.CompareTag("Slot"))
+        if (heldBlock == null)
         {
-            GameObject slot = slotCollider.gameObject;
+            Debug.LogError("No block is currently being held!");
+            return;
+        }
 
-            // Place the block in the slot
-            heldBlock.transform.SetParent(null);
-            heldBlock.transform.position = slot.transform.position;
-            heldBlock.GetComponent<Rigidbody2D>().isKinematic = false; // Re-enable physics
-            heldBlock = null; // Release the block
+        // Detect if the player is near a valid slot
+        Collider2D detectedCollider = Physics2D.OverlapCircle(transform.position, grabDistance);
+
+        if (detectedCollider == null)
+        {
+            Debug.Log("No valid slot detected.");
+            return;
+        }
+
+        GameObject detectedSlot = detectedCollider.gameObject;
+
+        // Check if the detected object has the "Slot" tag
+        if (detectedSlot.CompareTag("Slot"))
+        {
+            Debug.Log("Valid slot detected. Attempting to place block...");
+
+            // Before placing, check if the slot already has a block, and remove it
+            GameObject existingBlock = secondaryMemoryInstance.GetBlockInSlot(detectedSlot);
+            if (existingBlock != null)
+            {
+                secondaryMemoryInstance.RemoveBlockFromSlot(detectedSlot);
+                Debug.Log("Removed existing block from slot.");
+            }
+
+            // Now try to place the block
+            if (secondaryMemoryInstance.TryAddBlockToSlot(detectedSlot, heldBlock))
+            {
+                Debug.Log("Block successfully placed in the slot.");
+                heldBlock = null; // Release the block after placement
+            }
+            else
+            {
+                Debug.Log("Slot is full or placement failed.");
+            }
+        }
+        else
+        {
+            Debug.Log("Detected object is not a valid slot.");
         }
     }
 
+    private void HighlightSlot()
+    {
+        if (heldBlock == null) return;
+
+        // Detect if the player is near a valid slot (using the 'Slot' tag)
+        Collider2D detectedCollider = Physics2D.OverlapCircle(transform.position, grabDistance);
+
+        if (detectedCollider == null)
+        {
+            RemoveHighlight();
+            return;
+        }
+
+        GameObject detectedSlot = detectedCollider.gameObject;
+
+        // Check if the detected object has the "Slot" tag
+        if (detectedSlot.CompareTag("Slot"))
+        {
+            if (highlightedSlot != detectedSlot)
+            {
+                RemoveHighlight(); // Remove the previous highlight if needed
+                highlightedSlot = detectedSlot;
+
+                // Create and position the highlight border around the slot
+                highlightedBorder = Instantiate(highlightBorderPrefab, detectedSlot.transform.position, Quaternion.identity);
+                highlightedBorder.transform.SetParent(detectedSlot.transform); // Attach it to the slot
+            }
+        }
+        else
+        {
+            RemoveHighlight();
+        }
+    }
+
+    // private void TryPlaceBlock()
+    // {
+    //     if (heldBlock == null)
+    //     {
+    //         Debug.LogError("No block is currently being held!");
+    //         return;
+    //     }
+
+    //     // Detect if the player is near a valid slot (use the "Slot" layer)
+    //     Collider2D detectedCollider = Physics2D.OverlapCircle(transform.position, grabDistance, LayerMask.GetMask("Slot"));
+
+    //     if (detectedCollider == null)
+    //     {
+    //         Debug.Log("No valid slot detected.");
+    //         return;
+    //     }
+
+    //     GameObject detectedSlot = detectedCollider.gameObject;
+
+    //     // Check if the detected object has the "Slot" tag
+    //     if (detectedSlot.CompareTag("Slot"))
+    //     {
+    //         Debug.Log("Valid slot detected. Attempting to place block...");
+
+    //         // Try to place the block in the slot
+    //         if (secondaryMemoryInstance.TryAddBlockToSlot(detectedSlot, heldBlock))
+    //         {
+    //             Debug.Log("Block successfully placed in the slot.");
+    //             heldBlock = null; // Release the block after placement
+    //         }
+    //         else
+    //         {
+    //             Debug.Log("Slot is full or placement failed.");
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Detected object is not a valid slot.");
+    //     }
+    // }
+
+    // private void HighlightSlot()
+    // {
+    //     if (heldBlock == null) return;
+
+    //     // Detect if the player is near a valid slot (using the "Slot" layer)
+    //     Collider2D detectedCollider = Physics2D.OverlapCircle(transform.position, grabDistance, LayerMask.GetMask("slotLayer"));
+
+    //     if (detectedCollider == null)
+    //     {
+    //         RemoveHighlight();
+    //         return;
+    //     }
+
+    //     GameObject detectedSlot = detectedCollider.gameObject;
+
+    //     // Check if the detected object has the "Slot" tag
+    //     if (detectedSlot.CompareTag("Slot"))
+    //     {
+    //         if (highlightedSlot != detectedSlot)
+    //         {
+    //             RemoveHighlight(); // Remove the previous highlight if needed
+    //             highlightedSlot = detectedSlot;
+
+    //             // Create and position the highlight border around the slot
+    //             highlightedBorder = Instantiate(highlightBorderPrefab, detectedSlot.transform.position, Quaternion.identity);
+    //             highlightedBorder.transform.SetParent(detectedSlot.transform); // Attach it to the slot
+    //         }
+    //     }
+    //     else
+    //     {
+    //         RemoveHighlight();
+    //     }
+    // }
+
+
+    private void RemoveHighlight()
+    {
+        if (highlightedBorder != null)
+        {
+            Destroy(highlightedBorder); // Destroy the highlight border object
+            highlightedBorder = null;
+            highlightedSlot = null;
+        }
+    }
+
+    // For debug visualization of grab distance (in scene view)
     private void OnDrawGizmosSelected()
     {
-        // Visualize grab range
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, grabDistance);
     }
--- Assets/scripts/memoryManagement/SecondaryMemory.cs
+++ Assets/scripts/memoryManagement/SecondaryMemory.cs
@@ -1,29 +1,165 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class SecondaryMemory : MonoBehaviour
+public class secondaryMemory : MonoBehaviour
 {
-    public Transform[] slots; // Array of slot positions
-    private GameObject[] blocks; // Blocks currently in the slots
+    private Dictionary<GameObject, GameObject> slotToBlockMap = new Dictionary<GameObject, GameObject>();
 
-    private void Start()
-    {
-        blocks = new GameObject[slots.Length];
-    }
+    /// <summary>
+    /// Tries to add a block to the given slot.
+    /// </summary>
+    /// <param name="slot">The slot GameObject to place the block in.</param>
+    /// <param name="block">The block GameObject to place in the slot.</param>
+    /// <returns>True if the block was successfully added; otherwise, false.</returns>
 
-    public bool TryPlaceBlock(GameObject block, Transform player)
+    public bool TryAddBlockToSlot(GameObject slot, GameObject block)
     {
-        // Find the first empty slot
-        for (int i = 0; i < slots.Length; i++)
+        if (slot == null || block == null)
         {
-            if (blocks[i] == null)
-            {
-                // Place the block in the slot
-                blocks[i] = block;
-                block.transform.position = slots[i].position;
-                return true;
-            }
+            Debug.LogError("Slot or block is null.");
+            return false;
         }
 
-        return false; // No empty slot found
+        Debug.Log($"Attempting to place block: {block.name} into slot: {slot.name}");
+
+        // Check if the slot is already occupied
+        if (slotToBlockMap.ContainsKey(slot))
+        {
+            Debug.LogWarning("Slot is already occupied.");
+            return false;
+        }
+
+        // Add the block to the slot
+        slotToBlockMap[slot] = block;
+
+        // Parent the block to the slot for proper positioning and hierarchy
+        block.transform.SetParent(slot.transform);
+        block.transform.position = slot.transform.position;
+
+        Debug.Log("Block successfully added to slot.");
+        return true;
+    }
+
+    // public bool TryAddBlockToSlot(GameObject slot, GameObject block)
+    // {
+    //     if (slot == null || block == null)
+    //     {
+    //         Debug.Log("Slot or block is null.");
+    //         return false;
+    //     }
+
+    //     Debug.Log($"Attempting to place block: {block.name} into slot: {slot.name}");
+
+    //     // Check if the slot is already occupied
+    //     if (slotToBlockMap.ContainsKey(slot))
+    //     {
+    //         Debug.Log("Slot is already occupied.");
+    //         return false;
+    //     }
+
+    //     // Add the block to the slot
+    //     slotToBlockMap[slot] = block;
+
+    //     // Parent the block to the slot for proper positioning and hierarchy
+    //     block.transform.SetParent(slot.transform);
+    //     block.transform.position = slot.transform.position;
+
+    //     Debug.Log("Block successfully added to slot.");
+    //     return true;
+    // }
+
+
+    /// <summary>
+    /// Removes a block from the given slot.
+    /// </summary>
+    /// <param name="slot">The slot GameObject to remove the block from.</param>
+    /// <returns>The block GameObject if removed, otherwise null.</returns>
+    
+    public GameObject RemoveBlockFromSlot(GameObject slot)
+    {
+        if (slot == null)
+        {
+            Debug.LogError("Slot is null.");
+            return null; // Return null if the slot is invalid
+        }
+
+        if (!slotToBlockMap.ContainsKey(slot))
+        {
+            Debug.LogWarning("No block to remove from this slot.");
+            return null; // Return null if the slot is empty
+        }
+
+        GameObject block = slotToBlockMap[slot];
+        slotToBlockMap.Remove(slot);
+
+        // Detach the block from the slot
+        block.transform.SetParent(null);
+
+        Debug.Log($"Block {block.name} removed from slot {slot.name}.");
+        return block; // Return the removed block
+    }
+
+    // public GameObject RemoveBlockFromSlot(GameObject slot)
+    // {
+    //     if (slot == null)
+    //     {
+    //         Debug.Log("Slot is null.");
+    //         return null; // Return null if the slot is invalid
+    //     }
+
+    //     if (!slotToBlockMap.ContainsKey(slot))
+    //     {
+    //         Debug.Log("No block to remove from this slot.");
+    //         return null; // Return null if the slot is empty
+    //     }
+
+    //     GameObject block = slotToBlockMap[slot];
+    //     slotToBlockMap.Remove(slot);
+
+    //     // Detach the block from the slot
+    //     block.transform.SetParent(null);
+    //     Debug.Log($"Block {block.name} removed from slot {slot.name}.");
+    //     return block; // Return the removed block
+    // }
+
+
+    /// <summary>
+    /// Checks if a slot is occupied.
+    /// </summary>
+    /// <param name="slot">The slot to check.</param>
+    /// <returns>True if the slot is occupied; otherwise, false.</returns>
+    public bool IsSlotOccupied(GameObject slot)
+    {
+        return slot != null && slotToBlockMap.ContainsKey(slot);
+    }
+
+    /// <summary>
+    /// Retrieves the block currently in a slot.
+    /// </summary>
+    /// <param name="slot">The slot to retrieve the block from.</param>
+    /// <returns>The block GameObject if it exists; otherwise, null.</returns>
+    public GameObject GetBlockInSlot(GameObject slot)
+    {
+        if (slot == null || !slotToBlockMap.ContainsKey(slot))
+        {
+            return null;
+        }
+
+        return slotToBlockMap[slot];
+    }
+
+    /// <summary>
+    /// Clears all slots and blocks.
+    /// </summary>
+    public void ClearAllSlots()
+    {
+        foreach (var kvp in slotToBlockMap)
+        {
+            GameObject block = kvp.Value;
+            block.transform.SetParent(null);
+        }
+
+        slotToBlockMap.Clear();
+        Debug.Log("All slots cleared.");
     }
 }

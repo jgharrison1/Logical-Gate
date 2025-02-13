02d0559e61fc1ee81cc18f51aba2a95b6447ab59
jgharrison1 <jasongh3711@yahoo.com>
2025-02-12 23:29:58 -0600

    Squashed commit of the following:
    commit 586aa4f38da1e4d3ed58f86321decd8b93370d5a
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Wed Feb 12 21:56:30 2025 -0600
    
        Enemies
    
        Both enemies changed
    
    commit 716a51e7ae1f0f7fe2286dcb2aae58b968d5d794
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Wed Feb 12 20:34:35 2025 -0600
    
        Door Fixed
    
    commit a874bdf2d6fde5a6e18bc0ff53557871bde51e0a
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Wed Feb 12 20:26:13 2025 -0600
    
        Walking fixed!
    
        The walking is smooth now!
    
    commit 1e908c39cf935f00ec7c4b52774d0b77be2af477
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Wed Feb 12 20:03:43 2025 -0600
    
        Applying character to prefab
    
    commit 9f9280511c4d756fc341fe5712c23b6b92993cad
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Wed Feb 12 19:56:47 2025 -0600
    
        Jumping Anim
    
        Jumping Animation
    
    commit eecde80d35fbc15a73ad1b45b6d19d58577f176f
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Tue Feb 11 22:25:27 2025 -0600
    
        LGPL-K
    
        Sprites Replaced
    
    commit 3af9b370665f864dd386fc0ae76b7189975517df
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Tue Feb 11 15:38:37 2025 -0600
    
        Platforms/Walls started
    
        Started to replace the walls/platforms with the new sprites. Prefabs are made
        Currently only in LGPL-K
    
    commit 13c777d8989b4a8c2c5628196d4ed0dff50aa50f
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Sun Feb 9 14:14:18 2025 -0600
    
        Background Complete
    
        Background1000, tiled 100x100 on each level
        3000x3000 for menues
    
    commit a5700c67d5190cf658a5ad9f3e6df4ca09f3fde1
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Sat Feb 8 18:26:56 2025 -0600
    
        Start on Background
    
    commit d6ba145d45c15656d9b0186667976ae132b7cf18
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Sun Feb 2 14:49:08 2025 -0600
    
        Walking and Idle Animations
    
        The first animations of the new character
        only in LGPL-K
    
    commit 3bb7e181dea5008d8727a422d07bd1eae74a1d3f
    Author: FireMysty <125101747+FireMysty@users.noreply.github.com>
    Date:   Sat Jan 25 17:59:11 2025 -0600
    
        Character Start
    
    commit 9515e3981696229569dab59adb3969436cc5d5b7
    Author: calebEbenkamp <calebebenkamp10@gmail.com>
    Date:   Sat Jan 25 14:46:06 2025 -0600
    
        Draft scripts

---
+++ Assets/scripts/memoryManagement/PageTable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PageTable : MonoBehaviour
+{
+    public Transform[] slots; // Array of slot positions
+    private GameObject[] blocks;
+
+    private void Start()
+    {
+        blocks = new GameObject[slots.Length];
+    }
+
+    public bool TryPlaceBlock(GameObject block, Transform player)
+    {
+        // Similar to SecondaryMemory, manage placements
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (blocks[i] == null)
+            {
+                blocks[i] = block;
+                block.transform.position = slots[i].position;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

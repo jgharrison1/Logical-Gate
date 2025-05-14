3235f33309e6d3fe1a85a39fd560524900fc8321
jgharrison1 <jasongh3711@yahoo.com>
2025-05-14 18:19:45 -0500

    Update Sound System
    Added PlayerPrefs to soundMixerManager script to persist sound settings throughout game. There was an error with the electronic font as well that has been corrected as far as i can tell.

--- Assets/scripts/SoundMixerManager.cs
+++ Assets/scripts/SoundMixerManager.cs
@@ -2,23 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    public Slider masterSlider;
+    public Slider FXSlider;
+    public Slider musicSlider;
+
+    void Start() {
+        if (PlayerPrefs.GetFloat("masterLevel")!= 0){
+            SetMasterVolume(PlayerPrefs.GetFloat("masterLevel"));
+        }
+        if (PlayerPrefs.GetFloat("FXLevel")!= 0) {
+            SoundFXVolume(PlayerPrefs.GetFloat("fxLevel"));
+        }
+        if (PlayerPrefs.GetFloat("musicLevel")!= 0) {
+            SoundMusicVolume(PlayerPrefs.GetFloat("musicLevel"));
+        }
+    }
 
     public void SetMasterVolume(float level)
     {
         audioMixer.SetFloat("masterVolume", Mathf.Log10(level) * 20f); //This changes how volume scales from logarithmic scaling to linear
+        PlayerPrefs.SetFloat("masterLevel", level);
+        masterSlider.value = level;
     }
 
     public void SoundFXVolume(float level) 
     {
         audioMixer.SetFloat("soundFXVolume", Mathf.Log10(level) * 20f);
+        PlayerPrefs.SetFloat("FXLevel", level);
+        FXSlider.value = level;
     }
 
     public void SoundMusicVolume(float level) 
     {
         audioMixer.SetFloat("musicVolume", Mathf.Log10(level) * 20f);
+        PlayerPrefs.SetFloat("musicLevel", level);
+        musicSlider.value = level;
     }
 }
